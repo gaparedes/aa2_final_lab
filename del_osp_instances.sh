e9b8cea73c8c37d6c0173d9ca752e219d4bb5877
Gustavo A Paredes <gustavo.paredes@xerbit.com>
2021-10-14 18:09:36 -0400

    Advanced Ansible Course Final Deploy

---
+++ del_osp_instances.sh
@@ -0,0 +1,5 @@
+openstack server delete app1       --os-cloud=${GUID}-project
+openstack server delete app2       --os-cloud=${GUID}-project
+openstack server delete appdb1     --os-cloud=${GUID}-project
+openstack server delete frontend1  --os-cloud=${GUID}-project
+
